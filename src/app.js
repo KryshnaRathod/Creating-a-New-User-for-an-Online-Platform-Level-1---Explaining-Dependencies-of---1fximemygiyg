4b8bb410e839560970992c6477b7f84f5ce13a67
Krishna Rathod <88035445+KryshnaRathod@users.noreply.github.com>
2023-07-19 14:12:47 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,62 +2,36 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 
-// Importing products from products.json file
 const userDetails = JSON.parse(
   fs.readFileSync(`${__dirname}/data/userDetails.json`)
 );
 
-//Middlewares
+
 app.use(express.json());
 
-// Write POST endpoint for registering new user
-app.post('/api/v1/details', (req, res) => {
-  // Extract user details from request body
+
+
+app.post("/api/v1/details", (req, res) => {
+  const newId = userDetails[userDetails.length - 1].id + 1;
   const { name, mail, number } = req.body;
-
-  // Validate user data
-  if (!name || !mail || !number) {
-    return res.status(400).json({
-      status: 'Error',
-      message: 'Invalid user data'
-    });
-  }
-
-  // Generate the ID for the new user
-  const lastUser = userDetails[userDetails.length - 1];
-  const newUserId = lastUser ? lastUser.id + 1 : 1;
-
-  // Create the new user object
-  const newUser = {
-    id: newUserId,
-    name: name,
-    mail: mail,
-    number: number
-  };
-
-  // Add the new user to the userDetails array
+  const newUser = { id: newId, name, mail, number };
   userDetails.push(newUser);
-
-  // Write the updated user details to the JSON file
-  // (You can replace this step with your specific file handling code)
-  // fs.writeFileSync('./data/userDetails.json', JSON.stringify(userDetails, null, 2));
-
-  res.status(201).json({
-    status: 'Success',
-    message: 'User registered successfully',
-    data: {
-      newUser: newUser
+  fs.writeFile(
+    `${__dirname}/data/userDetails.json`,
+    JSON.stringify(userDetails),
+    (err) => {
+      res.status(201).json({
+        status: "Success",
+        message: "User registered successfully",
+        data: {
+          userDetails: newUser,
+        },
+      });
     }
-  });
+  );
 });
 
 
-
- 
-
-
-
-// GET endpoint for sending the details of users
 app.get("/api/v1/details", (req, res) => {
   res.status(200).json({
     status: "Success",
@@ -68,7 +42,6 @@ app.get("/api/v1/details", (req, res) => {
   });
 });
 
-// GET endpoint for sending the products to client by id
 app.get("/api/v1/userdetails/:id", (req, res) => {
   let { id } = req.params;
   id *= 1;
